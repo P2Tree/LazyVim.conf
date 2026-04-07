a1af308e53b1be815886dc2eed09bb0b98c961cc
P2Tree <dicksonliuming@gmail.com>
2026-04-07 15:04:44 +0800

    Add new Vim options for formatting and warnings

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,18 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.g.autoformat = true
+-- LayVim auto format (default: true)
+
+-- Deprecation warnings (default: false)
+vim.g.deprecation_warnings = true
+
+local opt = vim.opt
+
+opt.scrolloff = 5 -- (default: 4)
+opt.sidescrolloff = 40 -- (default: 5)
+opt.colorcolumn = "101" -- (default: none)
+opt.wrapscan = false -- (default: true)
+
+vim.g.snacks_animate = false
